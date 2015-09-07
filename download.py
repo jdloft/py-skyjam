01a126bb8881b7fcd0d8c6eb06e5694847c79d92
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-09-06 21:52:18 -0600

    Use underscore in fix

--- download.py
+++ download.py
@@ -13,7 +13,7 @@ def fix(path):
     # LINUX: / -> DIVISION SLASH (U+2215)
 
     path = path.replace(':', "\uFF1A")
-    path = path.replace('/', ':')
+    path = path.replace('/', '_')
     # path.replace('/', "\u2215")
 
     return path
